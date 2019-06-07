b452126863d274a89d2a62d9b952bdf3a7929fe4
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-06-07 11:05:46 +0100

    Create Install-Ftp.ps1

---
+++ Install-Ftp.ps1
@@ -0,0 +1,19 @@
+# Install the Windows feature for FTP
+
+Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
+
+Install-WindowsFeature Web-Server -IncludeAllSubFeature  -IncludeManagementTools
+
+Import-Module WebAdministration
+
+# Create the FTP site
+
+New-Item -Path "D:\" -Name "FTProot" -ItemType "directory"
+
+$FTPSiteName = 'Dtek-Training-FTP-Site'
+
+$FTPRootDir = 'D:\FTPRoot'
+
+$FTPPort = 21
+
+New-WebFtpSite -Name $FTPSiteName -Port $FTPPort -PhysicalPath $FTPRootDir
