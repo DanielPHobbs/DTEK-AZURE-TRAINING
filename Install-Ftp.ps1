98bdcc80684b0fbb8c7821b02fb853d4578c75ec
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2019-07-01 18:25:04 +0100

    Update Install-Ftp.ps1

--- Install-Ftp.ps1
+++ Install-Ftp.ps1
@@ -1,6 +1,6 @@
 # Install the Windows feature for FTP
 Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
-Install-WindowsFeature Web-Server -IncludeAllSubFeature  IncludeManagementTools
+Install-WindowsFeature Web-Server -IncludeAllSubFeature  -IncludeManagementTools
 Import-Module WebAdministration
 # Create the FTP site
 $FTPSiteName = 'Default FTP Site'
